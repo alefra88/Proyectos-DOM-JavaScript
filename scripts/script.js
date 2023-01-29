bdcafee182c8c775fed9514082496920d9fe30e1
alefra88 <efra.rlz@gmail.com>
2023-01-29 06:28:05 -0600

    reloj

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 import hamburguerMenu from "./hamburguer_menu.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    hamburguerMenu(".panel-btn", ".panel",".menu a");
+    hamburguerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
 });
