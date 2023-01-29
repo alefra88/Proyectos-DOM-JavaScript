7f5ae148f2f90f7cbe5b6cb972dfbf917bdac9f8
alefra88 <efra.rlz@gmail.com>
2023-01-29 06:16:19 -0600

    creando el reloj

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 import hamburguerMenu from "./hamburguer_menu.js";
-
+import { digitalClock, alarm } from "./reloj.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    hamburguerMenu(".panel-btn", ".panel",".menu a");
+    hamburguerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
 });
