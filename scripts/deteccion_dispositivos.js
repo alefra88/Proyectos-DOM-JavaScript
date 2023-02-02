cb41332e1824997d79ac59db89fd31aae25255f4
alefra88 <efra.rlz@gmail.com>
2023-02-02 02:28:26 -0600

    deteccion de dispositivos User Agent

---
+++ scripts/deteccion_dispositivos.js
@@ -0,0 +1,63 @@
+const d = document,
+    n = navigator,
+    ua = n.userAgent;
+export default function userDevice(id) {
+    const $id = d.getElementById(id),
+        isMobile = {
+            android: () => ua.match(/android/i),
+            ios: () => ua.match(/iphone|ipad|ipod/i),
+            windows: () => ua.match(/windows phone/),
+            any: function () {
+                return this.android() || this.ios() || this.windows();
+            },
+        },
+        isDesktop = {
+            linux: () => ua.match(/linux/i),
+            mac: () => ua.match(/mac os/i),
+            windows: () => ua.match(/windows nt/i),
+            any: function () {
+                return this.linux() || this.mac() || this.windows();
+            },
+        },
+        isBrowser = {
+            chrome: () => ua.match(/chrome/i),
+            safari: () => ua.match(/safari/i),
+            firefox: () => ua.match(/firefox/i),
+            opera: () => ua.match(/opera|opera mini/i),
+            ie: () => ua.match(/msie|iemobile/i),
+            edge: () => ua.match(/edge/i),
+            any: function () {
+                return (
+                    this.chrome() ||
+                    this.safari() ||
+                    this.firefox() ||
+                    this.opera() ||
+                    this.ie() ||
+                    this.edge()
+                );
+            },
+        };
+    $id.innerHTML = `
+        <ul>
+        <li>User Agent:<b>${ua}</b></li>
+        <li>Plataforma: <b>${
+            isMobile.any() ? isMobile.any() : isDesktop.any()
+        }</b></li>
+        <li>Navegador: <b>${isBrowser.any()}</b></li>
+        </ul>
+        `;
+    // contenido exclusivo
+    if (isBrowser.chrome()) {
+        $id.innerHTML += `<p><mark>Este contenido solo se ve en Chrome</mark></p>`;
+    }
+    if (isBrowser.firefox()) {
+        $id.innerHTML += `<p><mark>Este contenido solo se ve en Firefox</mark></p>`;
+    }
+    if (isBrowser.linux()) {
+        $id.innerHTML += `<p><mark>Descarga nuestra app para linux</mark></p>`;
+    }
+    // Redirecciones
+    if (isMobile.android()) {
+        window.location.href = "https.//github.com/alefra88";
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ import scrollTopButton from "./btn_scroll.js";
 import darkTheme from "./dark_theme.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./pruebaResponsive.js";
+import userDevice from "./deteccion_dispositivos.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -31,6 +32,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.948104639214!2d-99.50752898536851!3d17.557657287976827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cbedc74b5d4081%3A0x219e3dda0dc43ba5!2sPalmera%20bifurcada!5e0!3m2!1ses!2smx!4v1675254955304!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     );
     responsiveTester("responsive-tester");
+    userDevice("user-device")
 });
 
 d.addEventListener("keydown", (e) => {
