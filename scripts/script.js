157842212db63d3aa17b9b6e8b000cb7cfe5b1d3
alefra88 <efra.rlz@gmail.com>
2023-02-03 01:11:16 -0600

    creando mensajes de deteccion de estado de red

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ import darkTheme from "./dark_theme.js";
 import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./pruebaResponsive.js";
 import userDevice from "./deteccion_dispositivos.js";
+import networkStatus from "./deteccion_de_estado.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -32,7 +33,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.948104639214!2d-99.50752898536851!3d17.557657287976827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cbedc74b5d4081%3A0x219e3dda0dc43ba5!2sPalmera%20bifurcada!5e0!3m2!1ses!2smx!4v1675254955304!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     );
     responsiveTester("responsive-tester");
-    userDevice("user-device")
+    userDevice("user-device");
 });
 
 d.addEventListener("keydown", (e) => {
@@ -41,3 +42,4 @@ d.addEventListener("keydown", (e) => {
 });
 
 darkTheme(".dark-theme-btn", "dark-mode");
+networkStatus();
