b7f6d562b014a0a7d4b28d011c6e2234025d9258
alefra88 <efra.rlz@gmail.com>
2023-02-04 06:34:55 -0600

    cambios mañanaeros

--- scripts/filtro_busquedas.js
+++ scripts/filtro_busquedas.js
@@ -3,7 +3,16 @@ const d = document;
 const searchFilters = (input, selector) => {
     d.addEventListener("keyup", (e) => {
         if (e.target.matches(input)) {
-            console.log(e.key);
+            // console.log(e.key);
+            if (e.key === "Escape") e.target.value = "";
+
+            d.querySelectorAll(selector).forEach(
+                (el) =>
+                    el.textContent.toLowerCase().includes(e.target.value)
+                        ? el.classList.remove("filter")
+                        : el.classList.add("filter")
+                // const text = node.textContent;
+            );
         }
     });
 };
